452f582792d05f8dd7faa0d34905675e8e73b586
Kobe <87835153+KbeDemy@users.noreply.github.com>
2025-12-09 21:47:50 +0100

    Refactor MotorDriver constructor and pin definitions
    Refactor MotorDriver constructor and clean up comments.

--- code/proof of concepts/hbridge/TB6612.h
+++ code/proof of concepts/hbridge/TB6612.h
@@ -4,25 +4,27 @@
 
 #include <Arduino.h>
 
+enum MotorSelect {
+    MOTOR_A,
+    MOTOR_B,
+};
+
 
 class MotorDriver {
 public:
     
-    MotorDriver(uint8_t pmwA, uint8_t in1A, uint8_t in2A, 
-                uint8_t pmwB, uint8_t in1B, uint8_t in2B);
+    MotorDriver(uint8_t pmwA, uint8_t in1A, uint8_t in2A, uint8_t pmwB, uint8_t in1B, uint8_t in2B);
   
     void motor_init(void);
     
     void set_speed(int16_t motor_a, int16_t motor_b);
 
 
-
 private:
     
     uint8_t pwmA_pin, pwmB_pin; // PWM pins for motor A and B
-    uint8_t in1A_pin, in2A_pin; // Direction control pins for motor A & B
-    uint8_t in1B_pin, in2B_pin; // Direction control pins for motor A & B
-    uint8_t stby_pin; // Standby pin
+    uint8_t in1A_pin, in2A_pin; // Direction control pins for motor A 
+    uint8_t in1B_pin, in2B_pin; // Direction control pins for motor B
 
     uint8_t ChannelA, ChannelB; // PWM channels for motor A and B
     uint32_t pwm_freq; // PWM frequency in Hz (1000-10000) 5000 optimal with finetuning
